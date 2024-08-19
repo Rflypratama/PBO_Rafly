aed458ae011d4e88dfbb2d2a99e852048b336441
Rflypratama <raflipratama0501@gmail.com>
2024-08-19 08:45:13 +0700

    Add files via upload

---
+++ Latihan1.1.php
@@ -0,0 +1,14 @@
+Class Mobil
+{
+    var $jumlahRoda=4;
+    var $warna="Merah";
+    var $bahanBakar="Pertamax";
+    var $harga=120000000;
+    var $merek='A';
+}
+
+
+
+$objekBMW = new Mobil;
+$objekTesla = new Mobil;
+$objekAudi = new Mobil;
---
+++ latihan1.2.php
@@ -0,0 +1,16 @@
+Class Mobil
+{
+    var $jumlahRoda=4;
+    var $warna="Merah";
+    var $bahanBakar="Pertamax";
+    var $harga=120000000;
+    var $merek='A';
+
+    public function statusHarga ()
+    {
+        if ($this->harga > 50000000) $status = 'Mahal';
+        else $status = 'Murah';
+        return $status;
+    }
+     
+}
